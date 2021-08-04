0ada681c7025e38dbfdb0648ad7eacb0e1509533
Erling Devold <erling@devold.tech>
2021-08-04 15:43:56 +0200

    debug

--- app/main.py
+++ app/main.py
@@ -78,6 +78,7 @@ def get_sensordata():
     q = SensorData.query.order_by(SensorData.id).all()
     data = {}
     for item in q:
+        print(item.time)
         data[item.id] = {"temperature": item.temperature, "humidity": item.humidity, "pm10": item.pm10,
                          "pm25": item.pm25, "northing": item.northing, "easting": item.easting}
 
